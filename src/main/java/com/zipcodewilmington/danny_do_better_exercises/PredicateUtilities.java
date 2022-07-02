aff1291f4a4cd6c63f5050e5d164425c729aa944
Gamze <gamze@zipcodersmbp81.myfiosgateway.com>
2022-07-02 12:33:52 -0400

    completed predicate

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
@@ -10,18 +10,22 @@ public class PredicateUtilities {
      * @return true if `x` is greater than `y`
      */
     public Boolean isGreaterThan(int x, int y) {
-
-        return x>y ;
+        if (x > y) {
+            return true;
+        }
+        return false;
     }
-
     /**
      * @param x
      * @param y
      * @return true if `x` is less than `y`
      */
     public Boolean isLessThan(int x, int y) {
+        if (x < y) {
+            return true;
+        }
+        return false;
 
-        return x<y;
     }
 
     /**
@@ -30,8 +34,11 @@ public class PredicateUtilities {
      * @return true if `x` is greater than or equal to `y`
      */
     public Boolean isGreaterThanOrEqualTo(int x, int y) {
+        if (x >= y) {
+            return true;
+        }
+        return false;
 
-        return x>=y;
     }
 
     /**
@@ -40,8 +47,11 @@ public class PredicateUtilities {
      * @return true if `x` is less than or equal to `y`
      */
     public Boolean isLessThanOrEqualTo(int x, int y) {
+        if (x <= y) {
+            return true;
+        }
+        return false;
 
-        return x<=y;
     }
 
 
